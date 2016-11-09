dfabfdb3a7ce7d7abac13e424c9d34bd31dce6d1
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2016-11-09 19:52:53 +0530

    Added views and user controller changes to take biodata

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   
   private #overriding params function
   def sign_up_params
-  	params.require(:user).permit(:name, :biodata, :email, :password, :password_confirmation)
+  	params.require(:user).permit(:name, :bio, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-  	params.require(:user).permit(:name, :biodata, :email, :password, :password_confirmation, :current_password)
+  	params.require(:user).permit(:name, :bio, :email, :password, :password_confirmation, :current_password)
   end
 end 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,14 @@
-class UserController < ApplicationController #everything inherited form application controller
-
+class UserController < ApplicationController
 	def home
+		@user = current_user
 	end
 
+	def update
+		@user = current_user
+		if @user.update(current_user)
+		    redirect_to @user
+		else
+		    render 'edit'
+		end
+	end
 end
