4e42466010c76666b7ff9d5a70e764c06a66f1e5
Tanya <tanyashrivastava05@gmail.com>
2016-11-17 01:00:55 +0530

    commit

--- app/controllers/ip_controller.rb
+++ app/controllers/ip_controller.rb
@@ -8,7 +8,6 @@ class IpController < ApplicationController
 		@user = current_user
 	end
 
-<<<<<<< HEAD
 	def ip_info
 		@user = current_user
 		@title = params[:title]
@@ -26,9 +25,5 @@ class IpController < ApplicationController
 	end
 
 	def license
-=======
-	def successful_upload
->>>>>>> d663d30592d505a316a80f3d5c46509af5ba4c5e
-		@user = current_user
 	end
 end
